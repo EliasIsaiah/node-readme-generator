11d84a5940a85aa8bb4e31a85922662053ec6cce
Elias Roussos <eliasIsaiah@outlook.com>
2020-05-23 10:03:05 -0400

    init install

---
+++ index.js
@@ -0,0 +1,34 @@
+// don't forget: npm init
+/* 
+The user will be prompted for their GitHub username and other information pertaining to the project the README is for.
+*/
+
+const questions = [
+
+    // this is going to an array of questions
+
+// What is your Project title? [string]
+// What is your Description? [string]
+// What is your Table of Contents? [Array:string] or an object
+// How to install: [string]
+// What is your Usage? [string]
+// What is your License? "MIT" [string]
+// who are the contributors? [Array:string]: ["Elias Roussos", "Dave Allen", "Michael Nos"] 
+// What are your Tests? [string] (markdown formatted?)
+// What are your Questions? see below:
+    // What is your User GitHub profile picture? [string] (image url?)
+    // What is your User GitHub email? [string]
+
+];
+
+function writeToFile(fileName, data) {
+    // where do I want the file to be placed? desktop? local directory? do I need to check?
+    // create a file with name fileName
+    // write to file fileName the data
+}
+
+function init() {
+    // initialize stuff that I need here
+}
+
+init();
